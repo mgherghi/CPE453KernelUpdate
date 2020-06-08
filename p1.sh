7b6ae8209cdbb28f4aad260b665c194a14abdc36
mgherghi <mgherghi@calpoly.edu>
2020-06-07 23:08:47 -0700

    basic script for CentOS and localmodconfig

---
+++ p1.sh
@@ -0,0 +1,226 @@
+
+#!/bin/bash
+# p01.sh
+# Gherghina, Mihai
+
+# Took me a while but I did this so that i could splice up
+# the input one at a time
+
+# for checking swapfile ######################################################
+FILE=/swapfile
+################################################################################
+
+# Pesky Dracut file ############################################################
+DRACUT=/etc/dracut.conf.d/xen.conf
+################################################################################
+
+# apps to check for install ####################################################
+pckarr=( autoconf automake binutils bison flex gcc gcc-c++ gdb glibc-devel \ libtool make pkgconf pkgconf-m4 pkgconf-pkg-config redhat-rpm-config \ rpm-build rpm-sign strace asciidoc byacc ctags diffstat git intltool ltrace \ patchutils perl-Fedora-VSP perl-generators pesign source-highlight \ 
+systemtap valgrind valgrind-devel cmake expect rpmdevtools rpmlint \
+jq wget perl ncurses-devel make gcc bc bison flex elfutils-libelf-devel \ openssl-devel )
+################################################################################
+
+# for checking swapfile ######################################################
+GPGKEYS=~/.gnupg
+################################################################################
+
+# check installed kernel #######################################################
+InstalledKernel=$(uname -r | cut -d. -f-2) # check installed kernel
+################################################################################
+
+# install dependencies #########################################################
+sudo yum update -y -q 
+for i in  ${pckarr[*]}
+ do
+  isinstalled=$(rpm -q $i)
+  if [ !  "$isinstalled" == "package $i is not installed" ]; then 
+    :   
+  else
+    sudo yum install $i -y -q
+  fi
+done
+################################################################################
+
+#Checks against mainline version ###############################################
+V=$(curl -s https://www.kernel.org/releases.json | \
+    jq '.latest_stable.version' -r)
+
+M="$(echo $(curl -s https://www.kernel.org/releases.json | \
+    jq '.latest_stable.version' -r)  \
+    | cut -d. -f1)"
+################################################################################
+
+##########################################################
+POSITIONAL=()
+for i in "$@"
+do
+key="$1"
+    case $key in
+        -g) Git=true
+            shift # past argument
+            ;;
+        -v) Version=true
+            shift # past argument
+            ;;
+        -h) Help=true
+            shift # past argument
+            ;;
+        -R) Reboot=true
+            shift # past argument
+            ;;
+        -D) Dir=true
+            TargetDir="$2"
+            shift
+            ;;
+        *) POSITIONAL+=("$1")
+            shift
+            ;;
+    esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+################################################################################
+
+################################################################################
+if [[ $Help ]]; then
+    echo "p01 - install current linux stable kernel
+    Installs current linux stable kernel source code into given Subdir
+    or ~/src/linux-stable by default.  p01 takes a number of options,
+    as described below.
+    -g         git clone source from kernel.org instead of wget or curl
+    -v         Version of new stable kernel but does not install it.
+    -h         Help should display options with examples.
+    -R         Reboot after download and install.
+    -D Subdir  Subdir is the fullpath of downloaded stable kernel source
+    "
+fi
+################################################################################
+
+
+# check to see if kernel is already installed ##################################
+if [[ $InstalledKernel == $V ]]; then
+    echo "linux-stable already installed"
+    exit 1
+fi
+################################################################################
+
+# Checks latest Version ########################################################
+if [[ $Version ]]; then
+    echo ""
+    echo 'Latest Stable Kernel Version is : '
+    echo $(curl -s https://www.kernel.org/releases.json | \
+    jq '.latest_stable.version' -r)
+    echo ""
+fi
+################################################################################
+
+#Checks to see if -D is passed ################################################
+if [[ $Dir ]]; then
+    mkdir $TargetDir && cd $TargetDir
+else
+    TargetDir=$HOME/src/ 
+    mkdir ${TargetDir}
+fi
+################################################################################
+
+# Building the swap ############################################################
+if [ ! -e "$FILE" ]; then
+    sudo fallocate -l 2G /swapfile 
+    sudo chmod 600 /swapfile 
+    sudo mkswap /swapfile 
+    sudo swapon /swapfile
+fi
+################################################################################
+
+# Dracut Check #################################################################
+if [ -e "$DRACUT" ]; then
+    sudo rm /etc/dracut.conf.d/xen.conf
+fi
+################################################################################
+
+# check for ccache  ############################################################
+if [[ $( ccache --version | grep -c 3.7.8 ) -lt 1 ]]; then
+    cd $TargetDir
+    wget "https://github.com/ccache/ccache/releases/download/v3.7.8/ccache-3.7.8.tar.xz"
+    wget "https://github.com/ccache/ccache/releases/download/v3.7.8/ccache-3.7.8.tar.xz.asc"
+
+    if [[ $(gpg --verify ccache-3.7.8.tar.xz.asc ccache-3.7.8.tar.xz 2>&1 \
+        | grep -c "Good signature .* \[ultimate\]") -lt 1 ]]; then
+        echo "Error: invalid source"
+        exit 1
+    else
+        echo "Good Source"
+    fi
+    tar xvf ccache-3.7.8.tar
+    cd $TargetDir/ccache-3.7.8
+    ./configure
+    make -j2
+    sudo make -j2 install
+fi
+################################################################################
+
+#Hard coding the keys , thank you stackexchange ################################
+if [ ! -d "$GPGKEYS" ]; then
+    gpg2 --locate-keys "torvalds@kernel.org" "gregkh@kernel.org" 
+    for trusted in "torvalds@kernel.org" "gregkh@kernel.org"; do
+        echo -e "5\ny\n" | gpg --command-fd 0 --expert --edit-key $trusted trust
+    done
+fi
+################################################################################
+
+#using the git method to download the kernel ###################################
+if [[ $Git ]]; then
+    #echo "git"
+    cd $TargetDir
+    git clone --depth 1 --single-branch --branch v$V \
+        'https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git' 
+    cd $TargetDir/linux-stable
+
+    if [[ $(git tag -v v$V 2>&1 \
+        | grep -c "Good signature .* \[ultimate\]") -lt 1 ]]; then
+        echo "Bad Source"
+        exit 1
+    else
+        echo "Good Source"
+    fi
+    
+    #sudo cp /boot/config-`uname -r` .config
+    yes '' | ccache make -j2 localmodconfig
+    ccache make -j2
+    ccache make -j2 modules
+    sudo make -j2 modules_install
+    sudo make -j2 install
+else  #use wget to download kernel###########################################
+    #echo "wget"
+    cd $TargetDir
+    wget "https://cdn.kernel.org/pub/linux/kernel/v${M}.x/linux-${V}.tar.xz"
+    wget "https://cdn.kernel.org/pub/linux/kernel/v${M}.x/linux-${V}.tar.sign"
+    xz -d -v linux-${V}.tar.xz
+
+    if [[ $(gpg --verify linux-$V.tar.sign linux-$V.tar 2>&1 \
+        | grep -c "Good signature .* \[ultimate\]") -lt 1 ]]; then
+        echo "Bad Source"
+        exit 1
+    else
+        echo "Good Source"
+    fi
+    sudo rm *.tar
+    sudo rm *.sign
+    tar xvf linux-${V}.tar
+    cd ${TargetDir}/linux-${V}
+
+    #sudo cp /boot/config-`uname -r` .config
+    yes '' | ccache make -j2 localmodconfig
+    ccache make -j2
+    ccache make -j2 modules
+    sudo make -j2 modules_install
+    sudo make -j2 install
+fi
+################################################################################
+
+# if -R is passed ##############################################################
+if [[ $Reboot ]]; then
+    sudo reboot
+fi
+################################################################################
+
+exit 0  # exit on success
